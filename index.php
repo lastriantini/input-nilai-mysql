a6759d44c1ae9820d57c765bc7fc98a8357e56dc
lastriantini <lastriantini@smkwikrama.sch.id>
2023-01-23 10:15:18 +0700

    mysql

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<head>
+    <title>tugas2</title>
+</head>
+<body>
+    <h1>BIODATA</h1>
+    <form method="post" action="tampil.php">
+        <label for="nis">NIS</label><br/>
+		<input type="text" name="nis"><br/><br/>
+
+		<label for="nama">Nama</label><br/>
+		<input type="text" name="nama"><br/><br/>
+
+        <label for="alamat">Alamat</label><br>
+        <input type="text" name="alamat"><br><br>
+
+        <label for="ttl">Tanggal Lahir</label><br>
+        <input type="date" name="ttl"><br><br>
+
+		<label for="umur">Umur</label><br/>
+		<input type="number" name="umur"><br/><br/>
+
+        <label for="sekolah">Sekolah</label><br>
+        <input type="text" name="sekolah"><br><br>
+
+        <label for="rombel">Rombel</label><br/>
+		<input type="text" name="rombel"><br/><br/>
+
+        <label for="rayon">Rayon</label><br/>
+		<input type="text" name="rayon"><br/><br/>
+
+        <h2>INPUT NILAI</h2>
+
+        <label for="np">Nilai Produktif</label> <br/>
+        <input type="number" name="np"><br><br>
+
+        <label for="nm">Nilai Matematika</label><br/>
+        <input type="number" name="nm"><br><br>
+
+        <label for="ni">Nilai Indonesia</label><br>
+        <input type="number" name="ni"> <br><br>
+
+        <label for="ns">Nilai Pipas</label> <br>
+        <input type="number" name="ns"><br><br>
+       
+        <label for="nd">Nilai Sunda</label> <br>
+        <input type="number" name="nd"><br><br>
+
+        <input type="submit" value="oke">
+
+	</form>
+
+</body>
---
+++ tampil.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="index.php">
+    
+<?php
+
+$server = mysqli_connect("localhost", "root", "", "latihan_xpplg");
+
+if ($server) {
+   echo "berhasil hore! <br>";
+} else {
+   echo "gagal:(";
+}
+
+echo "<b>BIODATA SISWA</b>";
+echo "<br><br>";
+
+$nis = $_POST ['nis'];
+echo "NIS : <b>$nis</b>";
+echo "<br> <br>";
+
+$nama = $_POST ['nama'];
+echo "Nama : <b>$nama</b>";
+echo "<br> <br>";
+
+$alamat = $_POST ['alamat'];
+echo "Alamat : <b>$alamat</b>";
+echo "<br> <br>";
+
+$ttl = $_POST ['ttl'];
+echo "Tanggal lahir: <b>$ttl</b>";
+echo "<br> <br>";
+
+$umur = $_POST ['umur'];
+echo "Umur : <b>$umur</b>";
+echo "<br> <br>";
+
+$sekolah = $_POST ['sekolah'];
+echo "sekolah : <b>$sekolah</b>";
+echo "<br> <br>";
+
+$rombel = $_POST ['rombel'];
+echo "rombel : <b>$rombel</b>";
+echo "<br> <br>";
+
+$rayon = $_POST ['rayon'];
+echo "Rayon : <b>$rayon</b>";
+echo "<br> <br>";
+
+$sql ="insert into profil (nis, nama, alamat, tanggallahir, umur, sekolah, rombel, rayon)
+values('$nis', '$nama', '$alamat', '$ttl', '$umur', '$sekolah', '$rombel', '$rayon')";
+
+if (mysqli_query($server, $sql)) {
+  echo "berhasil!!! <br> <br>";
+} else {
+  echo "gagla";
+}
+
+echo "<b>HASIL NILAI</b>";
+echo "<br> <br>";
+
+$np = $_POST ['np'];
+echo "Nilai Produktif : $np";
+echo "<br> <br>";
+
+$nm = $_POST ['nm'];
+echo "Nilai Matematika : $nm";
+echo "<br> <br>";
+
+$ni = $_POST ['ni'];
+echo "Nilai Indonesia : $ni";
+echo "<br> <br>";
+
+$ns = $_POST ['ns'];
+echo "Nilai Pipas : $ns";
+echo "<br> <br>";
+
+$nd = $_POST ['nd'];
+echo "Nilai Sunda : $nd";
+echo "<br> <br>";
+
+$np = $_POST ['np'];
+$nm = $_POST ['nm'];
+$ns = $_POST ['ns'];
+$ni = $_POST ['ni'];
+$ni = $_POST ['nd'];
+
+$sum = $np + $ni + $nm + $ns + $nd;
+$mean = $sum/5;
+echo "<b>rata rata nilai : $mean";
+echo "<br> <br>";
+
+echo "<b>nilai max : ";
+echo max ($np, $nm, $ni, $ns, $nd);
+echo "</b><br><br>";
+
+echo "<b>nilai min : ";
+echo min ($np, $ni, $nm, $ns, $nd);
+echo "</b><br><br>";
+
+if ($mean >= 90) {
+    echo "rata rata nilai anda adalah <b>A</b>";
+    echo "<br> <br>";
+} elseif ($mean >= 75) {
+    echo "rata rata nilai anda adalah <b>B</b>";
+    echo "<br> <br>";
+} elseif ($mean >= 65) {
+    echo "rata rata nilai anda adalah <b>C</b>";
+    echo "<br> <br>";
+} elseif ($mean < 70) {
+    echo "rata rata nilai anda adalah <b>D</b>";
+    echo "<br> <br>";
+}
+
+echo "NAMA KELOMPOK";
+echo "<br> <br>";
+
+echo "Alya nursyifa";
+echo "<br> <br>";
+
+echo "Awittia Ananda Oktaviani";
+echo "<br> <br>";
+
+echo "Lastriantini";
+echo "<br> <br>";
+
+echo "Siti Lubna Salsabila Muslimah";
+echo "<br> <br>";
+
+echo "Rainy Nur Baeti Putri";
+echo "<br> <br>";
+?>
+    <input type="submit" value="kembali">
+</body>
+</html>
